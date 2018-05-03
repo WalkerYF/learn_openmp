b2a49d7765bdcca6487e1eadf4554bb5bc39baed
walker <WalkerYF@yeah.net>
2018-05-04 06:25:04 +0800

    edit shell script

--- HW3-2/run.sh
+++ HW3-2/run.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
-SAVE_DATA_FILE=$1
-NUM_THREAD=$2
-DRAW_DATA_FILE="draw_image.py"
-export QT_DEVICE_PIXEL_RATIO=0
-export QT_AUTO_SCREEN_SCALE_FACTOR=1
-export PATH=/mnt/f/ProgramData/Anaconda3:$PATH
-make
+# cpp 文件名，嵌套层数，矩阵大小，是否优化（OPT,NOT_OPT)
+#输入输入
+# EXE=.exe
+g++$EXE $1 -o a.exe -fopenmp -std=c++11 -D SINGLE -D COLLAPSE_NUM=$2 -D MATRIX_SIZE=$3 -D $4
+g++$EXE $1 -o b.exe -fopenmp -std=c++11 -D PARALLEL -D COLLAPSE_NUM=$2 -D MATRIX_SIZE=$3 -D $4
+SAVE_DATA_FILE=$5
+
 # 串行时间的计算
 time=`./a.exe 10`
-echo 1,$time > $SAVE_DATA_FILE
+echo 1,$time >> $SAVE_DATA_FILE
 
 # 并行时间的计算
-for ((varible=2;varible<=8;varible = varible+2));do
-    echo "OMP_NUM_THREADS is  $OMP_NUM_THREADS"
+for ((varible=2;varible<=30;varible = varible+1));do
+    echo "OMP_NUM_THREADS: $varible | COLLAPSE:$2 | MATRIX_SIZE:$3 | OPT:$4 |"
     time=`./b.exe $varible`
     echo $varible,$time >> $SAVE_DATA_FILE
 done
-for ((varible=10;varible<=300;varible = varible+10));do
-    echo "OMP_NUM_THREADS is  $OMP_NUM_THREADS"
-    time=`./b.exe $varible`
-    echo $varible,$time >> $SAVE_DATA_FILE
+for ((varible=30;varible<=850;varible = varible+10));do
+   echo "OMP_NUM_THREADS: $varible | COLLAPSE:$2 | MATRIX_SIZE:$3 | OPT:$4 |"
+   time=`./b.exe $varible`
+   echo $varible,$time >> $SAVE_DATA_FILE
 done
-for ((varible=310;varible<=$NUM_THREAD;varible = varible+50));do
-    echo "OMP_NUM_THREADS is  $OMP_NUM_THREADS"
-    time=`./b.exe $varible`
-    echo $varible,$time >> $SAVE_DATA_FILE
-done
-
-python.exe $DRAW_DATA_FILE $SAVE_DATA_FILE
--- HW3-2/run_all.sh
+++ HW3-2/run_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MATRIX_SIZE=1200
 SAVE_FILE_SIZE=all_time.csv
-./run2.sh matrix_product.cpp 1 $MATRIX_SIZE OPT $SAVE_FILE_SIZE 
-./run2.sh matrix_product.cpp 2 $MATRIX_SIZE OPT $SAVE_FILE_SIZE
-./run2.sh matrix_product.cpp 3 $MATRIX_SIZE OPT $SAVE_FILE_SIZE
-./run2.sh matrix_product.cpp 1 $MATRIX_SIZE NOT_OPT $SAVE_FILE_SIZE 
-./run2.sh matrix_product.cpp 2 $MATRIX_SIZE NOT_OPT $SAVE_FILE_SIZE
-./run2.sh matrix_product.cpp 3 $MATRIX_SIZE NOT_OPT $SAVE_FILE_SIZE
+./run.sh matrix_product.cpp 1 $MATRIX_SIZE OPT $SAVE_FILE_SIZE 
+./run.sh matrix_product.cpp 2 $MATRIX_SIZE OPT $SAVE_FILE_SIZE
+./run.sh matrix_product.cpp 3 $MATRIX_SIZE OPT $SAVE_FILE_SIZE
+./run.sh matrix_product.cpp 1 $MATRIX_SIZE NOT_OPT $SAVE_FILE_SIZE 
+./run.sh matrix_product.cpp 2 $MATRIX_SIZE NOT_OPT $SAVE_FILE_SIZE
+./run.sh matrix_product.cpp 3 $MATRIX_SIZE NOT_OPT $SAVE_FILE_SIZE
