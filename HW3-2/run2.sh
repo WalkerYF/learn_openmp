d54a7a1e0fd075ed51d809574dc27972b0bc0e98
wyf in linux <walkeryf@yeah.net>
2018-05-03 23:54:02 +0800

    add the data

--- HW3-2/run2.sh
+++ HW3-2/run2.sh
@@ -7,17 +7,17 @@ g++$EXE $1 -o b.exe -fopenmp -std=c++11 -D PARALLEL -D COLLAPSE_NUM=$2 -D MATRIX
 SAVE_DATA_FILE=$5
 
 # 串行时间的计算
-time=`./a.exe 10`
-echo 1,$time >> $SAVE_DATA_FILE
+# time=`./a.exe 10`
+# echo 1,$time >> $SAVE_DATA_FILE
 
 # 并行时间的计算
-for ((varible=2;varible<=30;varible = varible+2));do
+for ((varible=3;varible<=30;varible = varible+2));do
     echo "OMP_NUM_THREADS: $varible | COLLAPSE:$2 | MATRIX_SIZE:$3 | OPT:$4 |"
     time=`./b.exe $varible`
     echo $varible,$time >> $SAVE_DATA_FILE
 done
-for ((varible=30;varible<=850;varible = varible+10));do
-    echo "OMP_NUM_THREADS: $varible | COLLAPSE:$2 | MATRIX_SIZE:$3 | OPT:$4 |"
-    time=`./b.exe $varible`
-    echo $varible,$time >> $SAVE_DATA_FILE
-done
+#for ((varible=30;varible<=850;varible = varible+10));do
+#    echo "OMP_NUM_THREADS: $varible | COLLAPSE:$2 | MATRIX_SIZE:$3 | OPT:$4 |"
+#    time=`./b.exe $varible`
+#    echo $varible,$time >> $SAVE_DATA_FILE
+#done
--- HW3-2/run_all.sh
+++ HW3-2/run_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MATRIX_SIZE=200
+MATRIX_SIZE=1200
 SAVE_FILE_SIZE=all_time.csv
 ./run2.sh matrix_product.cpp 1 $MATRIX_SIZE OPT $SAVE_FILE_SIZE 
 ./run2.sh matrix_product.cpp 2 $MATRIX_SIZE OPT $SAVE_FILE_SIZE
